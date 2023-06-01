b60334a95c135a35348605348edfe1488410acbf
Leandro Elverdi <leandroelverdi@gmail.com>
2023-06-01 16:07:42 -0300

    add new function

--- main.js
+++ main.js
@@ -1,45 +1,44 @@
+const valorCuota = (monto, cuotas) => {
+  return parseFloat(monto / cuotas).toFixed(2);
+};
+
 const calcularInteresDependiendo = (monto, cuotas) => {
-  let recargo = prompt("Se le cobran recargo? [S/N]");
-  while (recargo !== "S" && recargo !=="s" && recargo !== "N" && recargo !=="n") {
-    recargo = prompt("Se le cobran recargo? [S/N]");
+  let recargo = prompt("Se le cobran recargo? [S/N]").toLowerCase();
+  while (recargo !== "s" && recargo !== "n") {
+    recargo = prompt("Se le cobran recargo? [S/N]").toLowerCase();
   }
-  
-  if (recargo === "s" || recargo === "S") {
+
+  if (recargo === "s") {
     let porcentaje = parseFloat(prompt("Ingrese el interes"));
 
     let interes = parseFloat((monto * porcentaje) / 100);
 
-    let valorCuota = parseFloat(total / cuotas);
-    
     let total = parseFloat(monto + interes);
 
     let result = `El monto inicial es de $${monto} 
     Con un recargo del ${porcentaje}% queda en: $${total}
     Pagando en ${cuotas} cuotas
-    Cada cuota costará: $${valorCuota}`;
+    Cada cuota costará: $${valorCuota(total, cuotas)}`;
 
     return alert(result);
-
-  } else if (recargo === "n" || recargo === "N") {
-    console.log(valorCuota);
-    let result = ` El precio con interes es de: $
+  } else if (recargo === "n") {
+    let result = ` El precio final es de: $${monto}
     Cantidad de cuotas: ${cuotas}
-    Cada cuota costará: $${valorCuota}`;
+    Cada cuota costará: $${valorCuota(monto, cuotas)}`;
 
     return alert(result);
-
   }
 };
 
 //Calculadora de cuotas
-let salir = prompt("Calculadora de cuotas: \n[Enter] Iniciar\n[Q] Salir");
+let salir = prompt("Calculadora de cuotas: \n[Q] Salir").toLowerCase();
 
-while (salir !== "Q" && salir !== "q") {
+while (salir !== "q") {
   let monto = parseFloat(prompt("Ingrese el monto a pagar:"));
 
   let cuotas = parseFloat(prompt("Ingrese la cantidad de cuotas a pagar"));
 
   calcularInteresDependiendo(monto, cuotas);
 
-  salir = prompt("¿Calcular otro interes?\n[Enter] Continuar \n[Q] Salir");
+  salir = prompt("¿Calcular otro interes?\n[Enter] Continuar \n[Q] Salir").toLowerCase();
 }
