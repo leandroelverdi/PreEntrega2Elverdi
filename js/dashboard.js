724e3e0b142a2245aceba601a2b2101163ca924f
Leandro Elverdi <leandroelverdi@gmail.com>
2023-07-25 03:51:47 -0300

    dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,17 +1,18 @@
 const nameUser = localStorage.getItem("loggedInUser");
 const sideBar = document.querySelector("#sideBar");
-const h2 = document.createElement("h2");
-h2.innerText = `usuario: ${nameUser}`;
-sideBar.appendChild(h2);
-
-const bookings = document.querySelector("#bookings");
+const userName = document.querySelector("#userName");
+userName.innerText = `${nameUser}`;
 
 const cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 cart.forEach((cartItem) => {
-  const div = document.createElement("div");
-  div.innerHTML = `${cartItem.name}`
-  bookings.appendChild(div);
+  const tr = document.querySelector("#tr");
+  tr.innerHTML = `
+      <th scope="row">${cartItem.id}</th>
+      <td>${cartItem.name}</td>
+      <td>${cartItem.category}</td>
+      <td>${cartItem.price}</td>
+`;
 });
 
 const logoutButton = document.getElementById("logoutButton");
